b63455401535379c08571bb8cb1a77b1239520d7
Kallum Tolkien <kallumtolkien@gmail.com>
2021-01-08 15:37:20 +0000

    joinNames kata completed

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,17 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+    let listString = ''
+
+    for (let i=0; i<namesObj.length; i++) {
+        listString += namesObj[i].name;
+
+        if (i < namesObj.length - 2) {
+        listString += ', ';
+        } else if (i == namesObj.length - 2){
+        listString += ' & ';
+        }
+    }
+
+    return listString;
+};
 
 module.exports = joinNames;
